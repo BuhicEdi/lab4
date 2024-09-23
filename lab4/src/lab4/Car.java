3756be57fb1b33a8df4b5019560edf331c125ceb
unknown <edibuh-2@student.ltu.se>
2024-09-23 23:58:22 +0200

    Lab 4

--- lab4/src/lab4/Car.java
+++ lab4/src/lab4/Car.java
@@ -5,31 +5,31 @@ public class Car {
 	private static final double FUEL_TANK_VOLUME = 55.0;
 	private double fuelLevel = 0.0;
 	private double fuelPer10Km = 0.0;
-	
+
 	public Car(double fuelEfficiency) {
 		this.fuelPer10Km = fuelEfficiency;
 	}
-	
+
 	public double getFuelLevel() {
 		return fuelLevel;
 	}
-	
+
 	public void addFuel(double fuel) {
 		if (fuelLevel > FUEL_TANK_VOLUME) {
 			this.fuelLevel = 55.0;
 		} else {
-		this.fuelLevel += fuel;
+			this.fuelLevel += fuel;
 		}
 	}
-	
+
 	public void drive(double dist) {
 		if ((fuelLevel / fuelPer10Km) >= dist && dist > 0) {
 			this.fuelLevel = fuelLevel - (fuelPer10Km * dist);
 		} else if (dist > 0) {
 			this.fuelLevel = 0;
 		}
-			
+
 	}
-	
-	
+
+
 }
