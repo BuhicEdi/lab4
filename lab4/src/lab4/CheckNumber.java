a661c9897e660449fca8ff43b00076a51429e6fe
unknown <edibuh-2@student.ltu.se>
2024-09-23 11:02:12 +0200

    Lab 4

--- lab4/src/lab4/CheckNumber.java
+++ lab4/src/lab4/CheckNumber.java
@@ -1,16 +1,16 @@
 package lab4;
 
 public class CheckNumber {
-	
+
 	private double nbr1;
 	private double nbr2;
 	private double nbr3;
-	
-	
+
+
 	public void setNbr1(double setNbr1) {
 		nbr1 = setNbr1;
 	}
-	
+
 	public void setNbr2(double setNbr2) {
 		nbr2 = setNbr2;
 	}
@@ -18,15 +18,15 @@ public class CheckNumber {
 	public void setNbr3(double setNbr3) {
 		nbr3 = setNbr3;
 	}
-	
+
 	public boolean allTheSame() {
 		if (nbr1 == nbr2 && nbr2 == nbr3) {
 			return true;
 		} else {
-		return false;
+			return false;
 		}
 	}
-	
+
 	public boolean allDifferent() {
 		if (nbr1 != nbr2 && nbr2 != nbr3) {
 			return true;
@@ -34,7 +34,7 @@ public class CheckNumber {
 			return false;
 		}
 	}
-	
+
 	public boolean isSorted() {
 		if (nbr1 >= nbr2 && nbr2 >= nbr3) {
 			return true;
@@ -43,7 +43,7 @@ public class CheckNumber {
 		} else {
 			return false;
 		}
-		
+
 	}
-	
+
 }
