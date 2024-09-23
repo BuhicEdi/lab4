a8e811481222acdf1cd3d2dfee608457ca557b8e
unknown <edibuh-2@student.ltu.se>
2024-09-23 22:59:24 +0200

    Lab 4

---
+++ lab4/src/lab4/P4_5.java
@@ -0,0 +1,19 @@
+package lab4;
+
+public class P4_5 {
+
+	public static void main(String[] args) {
+		TallyCounter2 count = new TallyCounter2();
+		
+		count.count();
+		count.count();
+		count.count();
+		
+		count.setLimit(5);
+		
+		System.out.println(count.getValue());
+		
+		count.undo();
+	}
+
+}
